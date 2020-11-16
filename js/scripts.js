c0d6c43802f7af496a2001d150631fa81c2abb26
jmlinares2019 <jlinaresmar@gmail.com>
2020-11-16 01:19:46 +0100

    JS animations and mouse (click / hover) states

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
+/*--- top-tabs active state ---*/  
+
+$('.top-tab').click(function(e){
+    e.preventDefault();
+    $('.top-tab').removeClass('active');
+    $(this).toggleClass('active');
+});
+
 /*--- collapsable arrow rotation animation ---*/  
 
-$('.collapse-btn').click(function(){
+$('.aside-collapse').click(function(e){
+    e.preventDefault();
     $('.collapse-arrow', this).toggleClass('down');
+    $(this).toggleClass('open');
 });
